8d0bec4f087093a80a3687d1fa43e6c31c6b7d2e
Po-Hsun Chen <benjamin990126@hotmail.com>
2024-04-12 13:33:35 -0700

    clean and organize code for worldmap callback file

--- src/callbacks/worldmap.py
+++ src/callbacks/worldmap.py
@@ -1,13 +1,9 @@
-from dash import Dash, dcc, callback, Output, Input, html
-import dash_bootstrap_components as dbc
-import dash_vega_components as dvc
+from dash import callback, Output, Input
 import altair as alt
-import pandas as pd
-import geopandas as gpd
-import plotly.express as px
 
-from data.data import raw_data, processed_data, world, gdf
+from data.data import processed_data, world, gdf
 
+# Callback to update world map based on selection of metric and year
 @callback(
     Output('world', 'spec'),
     [Input('variable', 'value'),
@@ -17,39 +13,48 @@ def create_chart(variable, year_slider):
 
     gdf_filtered = gdf[gdf['Year'] == year_slider]
 
-    # hover effect
+    # Hover effect
     hover = alt.selection_point(
         fields=['Entity'], on='pointerover', empty=False
-        )
-    # click effect
+    )
+    # Click effect
     click = alt.selection_point(
         fields=['Entity'], name='select_region', on='click'
     )
 
-    non_missing_data = alt.Chart(gdf_filtered, width=600, height=800).mark_geoshape(
+    # Map data layer
+    non_missing_data = alt.Chart(gdf_filtered).mark_geoshape(
         stroke='#666666',
         strokeWidth=1
     ).project(
         'equalEarth'
     ).encode(
-        color=alt.Color(variable, 
-                        legend=alt.Legend(orient='none', legendX=10, legendY=460, direction='horizontal',
-                                          title=variable, gradientLength=300, 
-                                          labelLimit=500, titleLimit=500)),
-        # color=alt.Color(variable, legend=alt.Legend(orient='top-left')),
+        color=alt.Color(variable, legend=alt.Legend(
+                                        orient='none', 
+                                        legendX=10, legendY=460, 
+                                        direction='horizontal', 
+                                        title=variable, 
+                                        gradientLength=300, 
+                                        labelLimit=500, 
+                                        titleLimit=500
+                                    )
+        ),
         tooltip=['Entity', variable],
         stroke=alt.condition(hover, alt.value('white'), alt.value('#666666')), 
         order=alt.condition(hover, alt.value(1), alt.value(0))
+    ).properties(
+        width=600,
+        height=500,
     ).add_params(
         hover,
         click
     )
 
+    # Map background layer
     background_map = alt.Chart(world).mark_geoshape(color="lightgrey")
 
-    return(
-        (background_map + non_missing_data).properties(height=500).to_dict()
-    )
+    # Return map data + background layer
+    return((background_map + non_missing_data).to_dict())
 
 # Callback to update the dropdown box based on the click on the map
 @callback(
@@ -57,6 +62,8 @@ def create_chart(variable, year_slider):
     [Input('world', 'signalData')] 
 )
 def update_dropdown(clicked_region):
+
     if clicked_region and 'Entity' in clicked_region['select_region']:
         return clicked_region['select_region']['Entity'][0]
+    
     return processed_data['Entity'].unique()[0]
