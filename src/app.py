de62fa57887573847998127360883c5c39fc463e
Po-Hsun Chen <benjamin990126@hotmail.com>
2024-04-12 16:58:35 -0700

    perform chart formatting and fix pie chart title

--- src/app.py
+++ src/app.py
@@ -17,40 +17,41 @@ server = app.server
 
 # App layout of left side
 left_layout = dbc.Container([
-    metric_dropdown_label,
-    metric_dropdown,
+    dbc.Col(metric_dropdown_label, width=12),
+    dbc.Col(metric_dropdown, width=12),
     html.Br(),
-    year_slider_label,
-    year_slider,
-    world_map,
+    dbc.Col(year_slider_label, width=12),
+    dbc.Col(year_slider, width=12),
+    dbc.Col(world_map, width=12),
 ])
 
 # App layout of right side
 right_layout = dbc.Container([
-    country_dropdown_label,
-    country_dropdown,
+    dbc.Col(country_dropdown_label, width=12),
+    dbc.Col(country_dropdown, width=12),
     html.Br(),
     dbc.Row([
         dbc.Col(energy_consumption_pie_chart, width=6),
         dbc.Col(electricity_generation_pie_chart, width=6),
     ]),
     html.Br(),
-    dbc.Col(electricity_access_bar_chart),
+    dbc.Col(electricity_access_bar_chart, width=12),
     html.Br(),
-    dbc.Col(financial_flow_bar_chart),
+    dbc.Col(financial_flow_bar_chart, width=12),
     html.Br(),
     dbc.Row([
-        dbc.Col(gdp_per_capita_card),
-        dbc.Col(population_card)
+        dbc.Col(gdp_per_capita_card, width=6),
+        dbc.Col(population_card, width=6)
     ])
 ])
 
 # App layout combining left and right side
 app.layout = dbc.Container([
-    title,
+    dbc.Col(title, width=12),
+    html.Br(),
     dbc.Row([
-        dbc.Col(left_layout),
-        dbc.Col(right_layout),
+        dbc.Col(left_layout, width=6),
+        dbc.Col(right_layout, width=6),
     ]),
     dbc.Col(footer, width=12)
 ])
--- src/components/charts.py
+++ src/components/charts.py
@@ -4,9 +4,9 @@ import dash_vega_components as dvc
 world_map = dvc.Vega(id='world', opt={'actions': False}, spec={}, signalsToObserve=['select_region'])
 
 # Pie Charts
-energy_consumption_pie_chart = dvc.Vega(id='pie-chart', opt={'actions': False})
-electricity_generation_pie_chart = dvc.Vega(id='electricity-production', opt={'actions': False})
+energy_consumption_pie_chart = dvc.Vega(id='pie-chart', opt={'actions': False}, style={'width': '100%'})
+electricity_generation_pie_chart = dvc.Vega(id='electricity-production', opt={'actions': False}, style={'width': '100%'})
 
-# Bar Charts
+# Bar
 electricity_access_bar_chart = dvc.Vega(id='bar-chart-electricity', opt={'actions': False}, style={'width': '100%'})
 financial_flow_bar_chart = dvc.Vega(id='bar-chart-financial-flows', opt={'actions': False}, style={'width': '100%'})
