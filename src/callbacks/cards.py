201a67611ba0a8e6661793fda59620f63507a9c1
Po-Hsun Chen <benjamin990126@hotmail.com>
2024-04-12 13:56:23 -0700

    clean and organize code for individual cards callback file

--- src/callbacks/cards.py
+++ src/callbacks/cards.py
@@ -1,39 +1,9 @@
-from dash import Dash, dcc, callback, Output, Input, html
+from dash import callback, Output, Input
 import dash_bootstrap_components as dbc
-import dash_vega_components as dvc
-import altair as alt
-import pandas as pd
-import geopandas as gpd
-import plotly.express as px
 
-from data.data import raw_data, processed_data, world, gdf
+from data.data import processed_data, gdf
 
-header_style = {
-    'fontWeight': 'bold',
-    'color': '#245724',
-    'backgroundColor': '#e6f5e6',
-    'fontSize': '20px',
-    'fontFamily': 'Helvetica',
-    'padding': '15px',
-    'textAlign': 'center',
-    'border': 'none',
-    'boxShadow': 'none',
-    'outline': 'none'
-}
-
-body_style = {
-    # 'fontWeight': 'bold',
-    'color': '#f2fff2',
-    'backgroundColor': '#245724',
-    'fontSize': '22px',
-    'fontFamily': 'Helvetica',
-    'padding': '15px',
-    'textAlign': 'center',
-    'border': 'none',
-    'boxShadow': 'none',
-    'outline': 'none'
-}
-
+# Callback to update card values based on selected country
 @callback(
     [Output('gdp-card', 'children'),
      Output('population-card', 'children')],
@@ -44,6 +14,33 @@ def update_card(selected_entity):
     filtered_entity_data = processed_data[processed_data['Entity'] == selected_entity]
     gdp_per_capita = filtered_entity_data['gdp_per_capita'].iloc[0]   
     population = gdf[gdf["Entity"] == selected_entity]["pop_est"].iloc[-1]
+
+    # Specify card header styling
+    header_style = {
+        'fontWeight': 'bold',
+        'color': '#245724',
+        'backgroundColor': '#e6f5e6',
+        'fontSize': '20px',
+        'fontFamily': 'Helvetica',
+        'padding': '15px',
+        'textAlign': 'center',
+        'border': 'none',
+        'boxShadow': 'none',
+        'outline': 'none'
+    }
+
+    # Specify card body styling
+    body_style = {
+        'color': '#f2fff2',
+        'backgroundColor': '#245724',
+        'fontSize': '22px',
+        'fontFamily': 'Helvetica',
+        'padding': '15px',
+        'textAlign': 'center',
+        'border': 'none',
+        'boxShadow': 'none',
+        'outline': 'none'
+    }
 
     gdp_card = [
         dbc.CardHeader(f'GDP per Capita (USD)', style=header_style),
