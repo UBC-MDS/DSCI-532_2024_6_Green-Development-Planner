80c9eeea01511ad798e38af586d3f71be0988c13
Po-Hsun Chen <benjamin990126@hotmail.com>
2024-04-12 15:07:13 -0700

    test automatic update of deployment time in footer

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,4 +1,5 @@
 from dash import html
+from utils.utils import REPO_URL, get_repo_last_updated_time
 
 # Dashboard footer
 footer = html.P([
@@ -8,7 +9,7 @@ footer = html.P([
     html.Br(),  # Line break
     "Author: Ben Chen, Hayley Han, Ian MacCarthy, Joey Wu",
     html.Br(),  # Line break
-    "Latest update/deployment: April 6, 2024",
+    "Latest update/deployment: " + get_repo_last_updated_time().strftime("%B %d, %Y")),
     html.Br(),  # Line break
-    html.A('GitHub URL', href='https://github.com/UBC-MDS/DSCI-532_2024_6_Green-Development-Planner', target='_blank')
+    html.A('GitHub URL', href=REPO_URL, target='_blank')
 ], className="footer")
