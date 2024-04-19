e53e19f4e62535126daa23df8338a0c43b837d89
ianm99 <143557285+ianm99@users.noreply.github.com>
2024-04-19 15:00:56 -0700

    paired down the subtitle (#106)

--- src/components/subtitle.py
+++ src/components/subtitle.py
@@ -1,14 +1,18 @@
 import dash_bootstrap_components as dbc
+from dash import html
 
-subtitle = dbc.CardBody('This dashboard assesses potential for renewable energy development projects around the world, using data from the World Bank and International Energy Agency. \
-                         The map on the left shows values of one selected metric for all countries, while the charts on the right show values of each metric for one selected country.',
-                    style={
-                        'font-family': 'helvetica',
-                        'font-size': '16px',
-                        'color': '#f2fff2',
-                        'background-color':'#245724',
-                        'text-align': 'center',
-                        'width': '101.8%',
-                        'margin-left': '-11px'
-                    }
+subtitle = dbc.CardBody([
+    html.Br(),
+    html.H6('This dashboard evaluates global potential for renewable energy development projects.'),
+    html.P('The map on the left shows a chosen metric for all countries, while the charts on the right show details for a chosen country.'),
+                        ],
+    style={
+        'font-family': 'helvetica',
+        'font-size': '16px',
+        'color': '#f2fff2',
+        'background-color':'#245724',
+        'text-align': 'center',
+        'width': '101.8%',
+        'margin-left': '-11px'
+    }
 )
