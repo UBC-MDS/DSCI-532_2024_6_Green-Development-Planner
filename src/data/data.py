8154ae2136144be29a63cd2750a68991251a2d0d
Po-Hsun Chen <benjamin990126@hotmail.com>
2024-04-20 01:46:11 -0700

    change data for deployment testing

--- src/data/data.py
+++ src/data/data.py
@@ -2,24 +2,24 @@ import pandas as pd
 import geopandas as gpd
 
 # For development on local machine
-world = gpd.read_parquet("data/preprocessed/world_countries.parquet")
-gdf = gpd.read_parquet("data/preprocessed/preprocessed_gdf.parquet")
-consump_pie_data = pd.read_parquet("data/preprocessed/consump_pie_data.parquet")
-elec_pie_data = pd.read_parquet("data/preprocessed/elec_pie_data.parquet")
-access_to_electricity = pd.read_parquet("data/preprocessed/access_to_electricity.parquet")
-financial_flow = pd.read_parquet("data/preprocessed/financial_flow.parquet") 
-gdp_per_capita = pd.read_parquet("data/preprocessed/gdp_per_capita.parquet")
-population = pd.read_parquet("data/preprocessed/population_df.parquet")
+# world = gpd.read_parquet("data/preprocessed/world_countries.parquet")
+# gdf = gpd.read_parquet("data/preprocessed/preprocessed_gdf.parquet")
+# consump_pie_data = pd.read_parquet("data/preprocessed/consump_pie_data.parquet")
+# elec_pie_data = pd.read_parquet("data/preprocessed/elec_pie_data.parquet")
+# access_to_electricity = pd.read_parquet("data/preprocessed/access_to_electricity.parquet")
+# financial_flow = pd.read_parquet("data/preprocessed/financial_flow.parquet") 
+# gdp_per_capita = pd.read_parquet("data/preprocessed/gdp_per_capita.parquet")
+# population = pd.read_parquet("data/preprocessed/population_df.parquet")
 
 # For deployment on render
-# world = gpd.read_parquet("../data/preprocessed/world_countries.parquet")
-# gdf = gpd.read_parquet("../data/preprocessed/preprocessed_gdf.parquet")
-# consump_pie_data = pd.read_parquet("../data/preprocessed/consump_pie_data.parquet")
-# elec_pie_data = pd.read_parquet("../data/preprocessed/elec_pie_data.parquet")
-# access_to_electricity = pd.read_parquet("../data/preprocessed/access_to_electricity.parquet")
-# financial_flow = pd.read_parquet("../data/preprocessed/financial_flow.parquet") 
-# gdp_per_capita = pd.read_parquet("../data/preprocessed/gdp_per_capita.parquet")
-# population = pd.read_parquet("../data/preprocessed/population_df.parquet")
+world = gpd.read_parquet("../data/preprocessed/world_countries.parquet")
+gdf = gpd.read_parquet("../data/preprocessed/preprocessed_gdf.parquet")
+consump_pie_data = pd.read_parquet("../data/preprocessed/consump_pie_data.parquet")
+elec_pie_data = pd.read_parquet("../data/preprocessed/elec_pie_data.parquet")
+access_to_electricity = pd.read_parquet("../data/preprocessed/access_to_electricity.parquet")
+financial_flow = pd.read_parquet("../data/preprocessed/financial_flow.parquet") 
+gdp_per_capita = pd.read_parquet("../data/preprocessed/gdp_per_capita.parquet")
+population = pd.read_parquet("../data/preprocessed/population_df.parquet")
 
 # Specify coordinate reference system for geospatial data
 world.crs = 'EPSG:4326'
